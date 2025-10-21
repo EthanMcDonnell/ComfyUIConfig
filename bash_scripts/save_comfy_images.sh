c921d3b984f6bc924cdf2d3e68835483277350df
Ethan McDonnell <ethan@xelle.com>
2025-10-21 18:24:02 +1000

    update

--- bash_scripts/save_comfy_images.sh
+++ bash_scripts/save_comfy_images.sh
@@ -2,13 +2,11 @@ WORKSPACE_DIR="/workspace"
 COMFYUI_DIR="$WORKSPACE_DIR/ComfyUI"
 REPO_DIR="$WORKSPACE_DIR/ComfyUIConfig"
 
-COMFY_IMAGE_DIR="$COMFY_DIR/output"
+INPUT_COMFY_IMAGE_DIR="$COMFY_DIR/output"
 PYTHON_GOOGLE_DRIVE_SCRIPT="$REPO_DIR/gdrive.py"
 
 # (Google Drive folder IDs)
-declare -a IMAGES_GDRIVE_FOLDER=(
-    "1AS_-MutrsoLsMnsZp7R4Hoe9Sa_fcBhF" # Private GDRIVE Folder ID
-)
+IMAGES_GDRIVE_FOLDER="1AS_-MutrsoLsMnsZp7R4Hoe9Sa_fcBhF" # Private GDRIVE Folder ID
 
 if [ -z "$GDRIVE_SERVICE_ACCOUNT_JSON_B64" ]; then
     echo "Error: GDRIVE_SERVICE_ACCOUNT_JSON_B64 environment variable is not set." >&2
@@ -16,11 +14,4 @@ if [ -z "$GDRIVE_SERVICE_ACCOUNT_JSON_B64" ]; then
 fi
 
 echo "Uploading images to Google Drive..."
-if [ ${#IMAGES_GDRIVE_FOLDER[@]} -gt 0 ]; then
-    for folder_id in "${IMAGES_GDRIVE_FOLDER[@]}"; do
-        echo "Uploading files from $folder_id → Google Drive"
-        python3 "$PYTHON_GOOGLE_DRIVE_SCRIPT" upload "$GDRIVE_SERVICE_ACCOUNT_JSON_B64" "$COMFY_IMAGE_DIR" "$folder_id"
-    done
-else
-    echo "No image upload google drives specified."
-fi 
+python3 "$PYTHON_GOOGLE_DRIVE_SCRIPT" upload "$GDRIVE_SERVICE_ACCOUNT_JSON_B64" "$INPUT_COMFY_IMAGE_DIR" "$IMAGES_GDRIVE_FOLDER"
